88ad6a489903c2509f030f367f3b7228b3af2078
Peter Polidoro <peterpolidoro@gmail.com>
2015-05-29 14:38:00 -0400

    Initial attempt at programming protocol.

--- hybridizer/hybridizer.py
+++ hybridizer/hybridizer.py
@@ -4,6 +4,7 @@ from modular_device import ModularDevices
 from bioshake_device import BioshakeDevice
 from exceptions import Exception
 import os
+import time
 
 try:
     from pkg_resources import get_distribution, DistributionNotFound
@@ -93,6 +94,24 @@ class Hybridizer(object):
             'system' : {
                 'channel' : 7,
             },
+            'asp' : {
+                'channel' : 8,
+            },
+            'red' : {
+                'channel' : 9,
+            },
+            'green' : {
+                'channel' : 9,
+            },
+            'yellow' : {
+                'channel' : 10,
+            },
+            'blue' : {
+                'channel' : 11,
+            },
+            'wash' : {
+                'channel' : 13,
+            },
         }
 
     def _debug_print(self, *args):
@@ -107,23 +126,81 @@ class Hybridizer(object):
         except KeyError:
             raise HybridizerError('Unknown valve key.')
 
+    def set_valves_on(self, valve_key_list):
+        try:
+            channels = [self._valves[valve_key]['channel'] for valve_key in valve_key_list]
+            self._msc.set_channels_on(channels)
+        except KeyError:
+            raise HybridizerError('Unknown valve key.')
+
     def set_valve_off(self, valve_key):
         try:
             valve = self._valves[valve_key]
             channels = [valve['channel']]
             self._msc.set_channels_off(channels)
         except KeyError:
-            raise HybridizerError('Invalid valve key.')
+            raise HybridizerError('Unknown valve key.')
+
+    def set_valves_off(self, valve_key_list):
+        try:
+            channels = [self._valves[valve_key]['channel'] for valve_key in valve_key_list]
+            self._msc.set_channels_off(channels)
+        except KeyError:
+            raise HybridizerError('Unknown valve key.')
 
     def set_all_valves_off(self):
-        for valve in self._valves:
-            self.set_valve_off(valve)
+        valve_keys = self.get_valves()
+        set_valves_off(valve_keys)
 
     def get_valves(self):
         valve_keys = self._valves.keys()
         valve_keys.sort()
         return valve_keys
 
+    def set_valve_on_until(self, valve_key, percent):
+        try:
+            valve = self._valves[valve_key]
+            channels = [valve['channel']]
+            ain = valve['analog_input']
+            set_until_index = self._msc.set_channels_on_until(channels,ain,percent)
+            while not self._msc.is_set_until_complete(set_until_index):
+                percent_current = self._msc.get_analog_input(ain)
+                print(str(valve_key) + ' is at ' + str(percent_current) + '%, waiting to reach ' + str(percent) + '%')
+                time.sleep(1)
+            self._msc.remove_set_until(set_until_index)
+        except KeyError:
+            raise HybridizerError('Unknown valve key or valve does not have analog_input.')
+
+    def set_valves_on_until_serial(self, valve_key_list, percent):
+        for valve_key in valve_key_list:
+            self.set_valve_on_until(valve_key,percent)
+
+    def run(self,valve_key):
+        print('running ' + valve_key + '...')
+        self.set_valves_on(['primer',valve_key])
+        self.set_valve_on('system')
+        print('priming ' + valve_key + '...')
+        time.sleep(5)
+        self.set_valve_off('system')
+        print('aspirating ' + valve_key + '...')
+        time.sleep(5)
+        self.set_valve_off('primer')
+        self.set_valves_on(['quad1','quad2','quad3','quad4','quad5','quad6','asp'])
+        self.set_valve_on('system')
+        print('loading ' + valve_key + ' into syringes...')
+        time.sleep(20)
+        self.set_valve_off('system')
+        print('dispensing ' + valve_key + ' into microplate...')
+        time.sleep(10)
+        self.bsc.shake_on()
+        print('shaking...')
+        time.sleep(10)
+        self.bsc.shake_off()
+        self.set_valve_off('asp')
+        print('aspirating ' + valve_key + ' from microplate...')
+        time.sleep(20)
+        self.set_valve_off(valve_key)
+        print(valve_key + ' finished!')
 
 # -----------------------------------------------------------------------------------------
 if __name__ == '__main__':
